4e582aba75724ac1f2bd8fe4e7cda0225537651e
MartinaB91 <martinabjorlin@hotmail.com>
2022-02-03 19:15:53 +0000

    Changed how menu is printed and added else to if statement in function start_program.

--- run.py
+++ run.py
@@ -1,27 +1,28 @@
 
 def create_budget():
-    print("Lets create a new budget!")
+    print("Let's create a new budget!")
     budget_name = input("Give your budget a name: \n")
     print(
-        f"The name of your new budget is {budget_name}. Now lets enter the values for each category."
+        f"The name of your new budget is {budget_name}. Now let's enter the amount for each category."
     )
     sum_shopping = input("How much do you want to budget for shopping? \n")
     sum_food = input("How much do you want to budget for food? \n")
     sum_entertainment = input("How much do you want to budget for entertainment? \n")
     sum_other = input("How much do you want to budget for other things? \n")
     
-    print(f"Your budget is as follow: \nShopping: {sum_shopping:>15} \nFood: {sum_food:>19} \nEntertainment: {sum_entertainment:>10} \nOther things: {sum_other:>11}")
+    print(f"Your budget is as follows: \nShopping: {sum_shopping:>15} \nFood: {sum_food:>19} \nEntertainment: {sum_entertainment:>10} \nOther things: {sum_other:>11}")
      
 def start_program():
-    # Present user with options over what choises they have and what they can do in this program.
+    # Present user with menu options over what choises they have and what they can do in this program.
     print("Welcome to your budget program")
-    print("What do you want to do?")
-    print("1. Create a new budget")
-    print("2. Add purchase to ongoing budget")
-    print("3. Get a summary of budget")
-    print("4. End ongoing budget")
-
-    selected_option = input("Please enter the number of your chooise: \n")
+    menu_options = ("What do you want to do? \n"
+    "1. Create a new budget. \n"
+    "2. Add purchase to ongoing budget. \n"
+    "3. Get a summary of ongoing budget. \n"
+    "4. End ongoing budget. \n")
+    print(menu_options)
+    
+    selected_option = input("Please select a menu option (1-4): \n")
     # Depending on what choice the user choose it will lead to different "pages". Every page has its a own function.
     if selected_option == "1":
         create_budget()
@@ -31,8 +32,10 @@ def start_program():
         print(selected_option)
     elif selected_option == "4":
         print(selected_option)
-    elif selected_option == "5":
-        print(selected_option)
+    else:
+        print(f"{selected_option} is not a valid menu option. Please try again.")
+        selected_option = input("Please select a menu option (1-4): \n") #Todo: Add call to function to start over.
+
   
 
 start_program()
