3b2ab62f8353fad9d1c36f880d788994421bb606
MartinaB91 <martinabjorlin@hotmail.com>
2022-02-03 12:18:55 +0000

    Initial commit. Created function start_program which presents different options for the user.

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def start_program():
+    # Present user with options over what choises they have and what they can do in this program.
+    print("Welcome to your budget program")
+    print("What do you want to do?")
+    print("1. Create a new budget")
+    print("2. Add purchase to ongoing budget")
+    print("3. Get a summary of budget")
+    print("4. End ongoing budget")
+
+    user_want_to = input("Please enter the number of your chooise: \n")
+    # Depending on what choice the user choose it will lead to different "pages". Every page has its a own function.
+    if user_want_to == 1:
+        print(user_want_to)
+    elif user_want_to == 2:
+        print(user_want_to)
+    elif user_want_to == 3:
+        print(user_want_to)
+    elif user_want_to == 4:
+        print(user_want_to)
+    elif user_want_to == 5:
+        print(user_want_to)
+  
+start_program()
