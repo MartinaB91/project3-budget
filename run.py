4d52e127443910becbf6f0e0a424c92536733670
MartinaB91 <martinabjorlin@hotmail.com>
2022-02-03 17:20:14 +0000

    Changed name on variable and changed if statements to str insteads of int in function start_program. Created function create_budget.

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 
+def create_budget():
+    print("Lets create a new budget!")
+    budget_name = input("Give your budget a name: \n")
+    print(
+        f"The name of your new budget is {budget_name}. Now lets enter the values for each category."
+    )
+    sum_shopping = input("How much do you want to budget for shopping? \n")
+    sum_food = input("How much do you want to budget for food? \n")
+    sum_entertainment = input("How much do you want to budget for entertainment? \n")
+    sum_other = input("How much do you want to budget for other things? \n")
+    
+    print(f"Your budget is as follow: \nShopping: {sum_shopping:>15} \nFood: {sum_food:>19} \nEntertainment: {sum_entertainment:>10} \nOther things: {sum_other:>11}")
+     
 def start_program():
     # Present user with options over what choises they have and what they can do in this program.
     print("Welcome to your budget program")
@@ -8,17 +21,22 @@ def start_program():
     print("3. Get a summary of budget")
     print("4. End ongoing budget")
 
-    user_want_to = input("Please enter the number of your chooise: \n")
+    selected_option = input("Please enter the number of your chooise: \n")
     # Depending on what choice the user choose it will lead to different "pages". Every page has its a own function.
-    if user_want_to == 1:
-        print(user_want_to)
-    elif user_want_to == 2:
-        print(user_want_to)
-    elif user_want_to == 3:
-        print(user_want_to)
-    elif user_want_to == 4:
-        print(user_want_to)
-    elif user_want_to == 5:
-        print(user_want_to)
+    if selected_option == "1":
+        create_budget()
+    elif selected_option == "2":
+        print(selected_option)
+    elif selected_option == "3":
+        print(selected_option)
+    elif selected_option == "4":
+        print(selected_option)
+    elif selected_option == "5":
+        print(selected_option)
   
+
 start_program()
+
+
+
+
